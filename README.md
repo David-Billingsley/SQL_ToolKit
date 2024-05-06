173d91d2fb1e224daa5e098ceee6c26b65707618
dave <daveandtaybillingsley@gmail.com>
2024-05-06 14:54:44 -0700

    Added connection string check

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ To use the tool add toolkit "github.com/David-Billingsley/SQL_ToolKit" to your c
 
 And in your function put var data DataBaseTools.Data
 
-Currently this file is set up to use Windows authentication only.
+If a username and password is passed to the functions it will generate the user and password connection string.  If there is no user / password then the system will use the Windows Auth connection string.
 
 ## SQL_File_Read
   This function takes the following parameters fpath (filepath), server(SQL Server name), database ( database name).  It will read any file in the current directory, and attempt to pass the query into sql.  Checks to see if file is .sql suffix.  If the number of files sent is greater then 0 it will send a true message back if zero sends false back.
--- sql_tools.go
+++ sql_tools.go
@@ -18,25 +18,32 @@ type Data struct {
 // #region: SQL Insert
 // This function takes the server string and database string from the SQL_File_Import and then also takes the file from the
 // File path file reads the whole file as a string and send to the server
-func sql_send(query string, server string, database string) {
-	ConnStringQA := fmt.Sprintf("server=%s;user id=;database=%s;", server, database)
-
-	conn2, err := sql.Open("sqlserver", ConnStringQA)
+func sql_send(query string, server string, database string, user string, pass string) {
+	var ConnString string
+	// Checks for user to be passed, if no user is passed then generate the windows auth string
+	if len(user) > 0 {
+		ConnString = fmt.Sprintf("server=%s;user id=;database=%s;", server, database)
+	} else {
+		ConnString = fmt.Sprintf("server=%s;user id=%s;password=%s;database=%s;", server, user, pass, database)
+	}
+	conn, err := sql.Open("sqlserver", ConnString)
 	if err != nil {
 		log.Fatal("Open connection failed:", err.Error())
 	}
 
-	rows2, err := conn2.Query(query)
+	defer conn.Close()
+
+	rows, err := conn.Query(query)
 	if err != nil {
 		fmt.Println("Error reading records: ", err.Error())
 	}
-	defer rows2.Close()
+	defer rows.Close()
 
 }
 
 // #region: SQL File Find
 // This function takes a file path and searches that path for files to read and pass into the sql_send function.
-func (t *Data) SQL_File_Import(fpath string, server string, database string) (map[string]string, bool) {
+func (t *Data) SQL_File_Import(fpath string, server string, database string, user string, pass string) (map[string]string, bool) {
 	// Creates a map to hold the fileanems found below.
 	var filefound = make(map[string]string)
 	entries, err := os.ReadDir(fpath)
@@ -58,7 +65,7 @@ func (t *Data) SQL_File_Import(fpath string, server string, database string) (ma
 			// Insert filename with an index as the key
 			filefound[fmt.Sprintf("%d", count)] = e.Name()
 
-			sql_send(string(query), server, database)
+			sql_send(string(query), server, database, user, pass)
 		}
 	}
 
